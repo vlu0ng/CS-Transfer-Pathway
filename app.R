876f98bcde666b592f0653b91d5270e220b810d3
Vinh Luong <vinh.luong@uci.edu>
2016-08-29 10:31:01 -0700

    - Update sidebar description. - Update inline CSS. - Test materializeR in app. - Update infoBox. - Subset foradmission and formajor .csv. - Flip renderformattable ifelse.

--- app.R
+++ app.R
@@ -9,55 +9,49 @@ library(shinyjs)
 
 foradmission <- read_csv("foradmission.csv")
 formajor <- read_csv("formajor.csv")
+campuses <- list('Berkeley' = c(`Computer Science, B.S.` = 'bcsbs'), 
+                 'Davis' = c(`Computer Science, B.S.` = 'dcsbs'),
+                 'Irvine' = c(`Computer Science, B.S.` = 'icsbs'),
+                 'Los Angeles' = c(`Computer Science, B.S.` = 'lacsbs', 
+                                   `Computer Science & Engineering, B.S.` = 'lacsebs'),
+                 'Merced' = c(`Computer Science, B.S.` = 'mcsbs'),
+                 'Riverside' = c(`Computer Science, B.S.` = 'rcsbs'),
+                 'San Diego' = c(`Computer Science, B.S.` = 'sdcsbs', 
+                                 `Computer Science, B.A.` = 'sdcsba'),
+                 'Santa Barbara' = c(`Computer Science, B.S.` = 'sbcsbs'),
+                 'Santa Cruz' = c(`Computer Science, B.S.` = 'sacsbs'))
 
 ui <- dashboardPage(
   
-  dashboardHeader(title = "UC Transfer Pathways"),
-    
-  dashboardSidebar(
-      
-    shinyjs::useShinyjs(),
-    inlineCSS(list(.red = "font-weight: normal; padding-left: 20px ; padding-right: 20px",
-                   .blue= "font-weight: normal; padding-left: 12px")),
+dashboardHeader(title = "UC Transfer Pathways"),
+  
+dashboardSidebar(
+  
+  shinyjs::useShinyjs(),
+  
+  inlineCSS(list(.red = "font-weight: normal; padding-left: 20px; padding-right: 20px",
+                 .blue = "font-weight: normal; padding-left: 12px",
+                 .green = "font-weight: normal", 
+                 .orange = "font-weight: normal; padding-left: 19px")),
+  
+  sidebarMenu(
+    menuItem("Computer Science", tabName = "compsci", icon = icon("desktop")),
+    fluidRow(div(id = "myapp",
+        div(id = "program",
+          selectizeInput("programInput", 
+            HTML("<i>As you decide which campuses to apply to, please use this tool
+                  to tailor your coursework to the specific admission requirements
+                  at those UC campuses.<br><br>
 
-    sidebarMenu(
-      menuItem("Computer Science", tabName = "compsci", icon = icon("desktop")),
-      menuItem(HTML("As you decide which campuses<br>
-                 to apply to, please use this tool<br>
-                 to tailor your coursework to the<br>
-                 specific admission requirements<br>
-                 at those UC campuses.")),
-      
-      fluidRow(
-        div(id = "myapp",
-          div(id = "program",
-              selectizeInput("programInput", HTML("Use the dropdown menu below
-                                               to select ALL the campuses and
-                                               programs you're interested in:<br><br>"), 
-                     choices = list('Berkeley' = c(`Computer Science, B.S.` = 'bcsbs'), 
-                                    'Davis' = c(`Computer Science, B.S.` = 'dcsbs'),
-                                    'Irvine' = c(`Computer Science, B.S.` = 'icsbs'),
-                                    'Los Angeles' = c(`Computer Science, B.S.` = 'lacsbs', 
-                                                      `Computer Science & Engineering, B.S.` = 'lacsebs'),
-                                    'Merced' = c(`Computer Science, B.S.` = 'mcsbs'),
-                                    'Riverside' = c(`Computer Science, B.S.` = 'rcsbs'),
-                                    'San Diego' = c(`Computer Science, B.S.` = 'sdcsbs', 
-                                                    `Computer Science, B.A.` = 'sdcsba'),
-                                    'Santa Barbara' = c(`Computer Science, B.S.` = 'sbcsbs'),
-                                    'Santa Cruz' = c(`Computer Science, B.S.` = 'sacsbs')),
-                    selected = "bcsbs", 
-                    multiple = TRUE,
-                    options = list(placeholder = "Select program(s) here")
-              )
-          ),
-          div(id = "buttons",
-            actionButton("update", "Update"),
-            actionButton("reset", "Clear Selections")
-          )
-        )
-      )
-      
-  )),
+                  Use the dropdown menu below to select ALL the campuses and
+                  programs you're interested in.</i><br><br>"), 
+            choices = campuses, selected = "", multiple = TRUE,
+            options = list(placeholder = "Make selections here"))),
+        
+          column(3, div(id = "subButton", submitButton("Update"))), 
+          column(3, div(id = "action", actionButton("reset", "Reset")))
+    ))
+)),
   
   #Dashboard Body Outputs
   dashboardBody(
@@ -65,19 +59,15 @@ ui <- dashboardPage(
     # FormatTable Outputs 
     fluidRow(
       column(width = 6,
-        box(
-          title = "Shared Transfer Pathway for Admission", status = "primary",
-          width = NULL, solidHeader = TRUE,
-          formattableOutput("requiredadmissionOutput"))
-      ),
+        box(title = "Shared Transfer Pathway for Admission", status = "primary",
+            width = NULL, solidHeader = TRUE,
+            formattableOutput("requiredadmissionOutput"))),
       
       column(width = 6,
-         box(
-           title = "Additional Lower Division Courses Required for Majors", status = "info", 
-           width = NULL, solidHeader = TRUE,
-           formattableOutput("requiredmajorOutput"))
-      )
-    ), #fluidrow end bracket
+         box(title = "Additional Lower Division Courses Required for Majors", status = "info", 
+            width = NULL, solidHeader = TRUE,
+            formattableOutput("requiredmajorOutput")))
+    ),
       
     #infoBox: Make the Most of Your Time
     #infoBox: General Education Requirements
@@ -112,16 +102,7 @@ ui <- dashboardPage(
     #InfoBox: Print CS Transfer Pathway
     fluidRow(
       infoBox(tags$b("Links to Program Websites"), 
-              selectizeInput("linkInput", NA, choices = list(
-                'Berkeley' = c(`Computer Science, B.S.` = 'BCSBS', `Computer Science & Engineering, B.S.` = 'BCSEBS'),
-                'Davis' = c(`Computer Science, B.S.` = 'DCSBS'),
-                'Irvine' = c(`Computer Science, B.S.` = 'ICSBS'),
-                'Los Angeles' = c(`Computer Science, B.S.` = 'LACSBS', `Computer Science & Engineering, B.S.` = 'LACSEBS'),
-                'Merced' = c(`Computer Science, B.S.` = 'MCSBS'),
-                'Riverside' = c(`Computer Science, B.S.` = 'RCSBS'),
-                'San Diego' = c(`Computer Science, B.S.` = 'SDACSBS', `Computer Science, B.A.` = 'SDCSBA'),
-                'Santa Barbara' = c(`Computer Science, B.S.` = 'SBCSBS'),
-                'Santa Cruz' = c(`Computer Science, B.S.` = 'SCCSBS')),
+              selectizeInput("linkInput", NA, choices = campuses,
               multiple = TRUE, 
               options = list(
                 placeholder = 'Select your programs'
@@ -197,13 +178,14 @@ server <- function(input, output) {
   shinyjs::addClass("berkeley", "blue")
   shinyjs::addClass("myapp", "red")
   shinyjs::addClass("instructions", "red")
-  shinyjs::addClass("buttons", "blue")
+  shinyjs::addClass("subButton", "green")
+  shinyjs::addClass("action", "orange")
 
   observe({
     toggleState("update", !is.null(input$programInput) && input$programInput != "")
   })
   
-  observeEvent(input$reset, {
+  observeEvent(input$programInput, {
     reset("myapp")
   })
 }
